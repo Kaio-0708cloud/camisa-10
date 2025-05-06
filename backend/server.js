5bbdb11f89f7d61e2287ba618204331d3934a76c
Kaio Vitor Santos Queiroz <123708201+Kaio-0708@users.noreply.github.com>
2025-05-06 15:41:50 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
 
 app.post("/create-checkout-session", async (req, res) => {
   const items = req.body.items;
